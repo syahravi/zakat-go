ba6c92efa4119321b97e8e9f4a22b132793ffea7
Syahravi <syahravi.id@gmail.com>
2023-08-30 12:45:36 +0700

    create

--- app/Http/Controllers/Admin/ZakatProduktifController.php
+++ app/Http/Controllers/Admin/ZakatProduktifController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\File;
+use App\Models\ZakatProduktif;
+use App\Models\ZakatSekarang;
 use Illuminate\Http\Request;
 
 class ZakatProduktifController extends Controller
@@ -12,8 +15,40 @@ class ZakatProduktifController extends Controller
         return view('admin.zakat-produktif.index');
     }
 
-    public function show()
+    public function create()
     {
-        // return view('admin.zakat-sekarang.show');
+        return view('admin.zakat-produktif.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'amount' => 'required|numeric',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        
+        $thumbnailPath = $request->file('thumbnail')->store('thumbnails', 'public');
+        $thumbnailFile = File::create([
+            'name' => now()->format('Ymd'),
+            'filename' => $request->file('thumbnail')->getClientOriginalName(),
+            'path' => $thumbnailPath,
+            'extension' => $request->file('thumbnail')->getClientMimeType(),
+        ]);
+
+        // Simpan data ZakatProduktif ke dalam database
+        ZakatProduktif::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'business' => $request->business,
+            'amount' => $request->amount,
+            'thumbnail_id' => $thumbnailFile->id,
+            'type_id' => 1,
+            'mustahik_id' => auth()->user()->id,
+            'amil_id' => auth()->user()->id,
+            'admin_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('admin.dashboard.zakat-produktif.index');
     }
 }
--- app/Http/Controllers/Admin/ZakatSekarangController.php
+++ app/Http/Controllers/Admin/ZakatSekarangController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\File;
+use App\Models\ZakatSekarang;
 use Illuminate\Http\Request;
 
 class ZakatSekarangController extends Controller
@@ -19,5 +21,36 @@ class ZakatSekarangController extends Controller
     public function create()
     {
         return view('admin.zakat-sekarang.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'amount' => 'required|numeric',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        
+        $thumbnailPath = $request->file('thumbnail')->store('thumbnails', 'public');
+        $thumbnailFile = File::create([
+            'name' => now()->format('Ymd'),
+            'filename' => $request->file('thumbnail')->getClientOriginalName(),
+            'path' => $thumbnailPath,
+            'extension' => $request->file('thumbnail')->getClientMimeType(),
+        ]);
+
+        // Simpan data ZakatSekarang ke dalam database
+        ZakatSekarang::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'amount' => $request->amount,
+            'thumbnail_id' => $thumbnailFile->id,
+            'type_id' => 1,
+            'mustahik_id' => auth()->user()->id,
+            'amil_id' => auth()->user()->id,
+            'admin_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('admin.dashboard.zakat-sekarang.index');
     }
 }
