f2c8f88ef71692a0c739e2b0ba03e7abf1458018
Syahravi <syahravi.id@gmail.com>
2023-08-30 13:16:49 +0700

    Zakat Sekarang v1

--- resources/views/admin/zakat-sekarang/create.blade.php
+++ resources/views/admin/zakat-sekarang/create.blade.php
@@ -3,31 +3,36 @@
 @section('content')
     <!-- Main Content -->
     <main class="max-w-screen-xl flex-auto m-10"> <!-- Tambahkan kelas "flex justify-end" -->
-        <form action="" method="POST" enctype="multipart/form-data">
+        <h1 class="text-2xl font-semibold mb-4">Buat Zakat Sekarang</h1>
+        <form action="{{ route('admin.dashboard.zakat-sekarang.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
+            <div class="my-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Judul</label>
+                <input
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name" name="name" type="text" placeholder="Judul">
+            </div>
+            <div class="my-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Deskripsi</label>
+                <textarea
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="description" name="description" placeholder="Deskripsi"></textarea>
+            </div>
+            <div class="my-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="amount">Jumlah</label>
+                <input
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="amount" name="amount" type="number" placeholder="Jumlah">
+            </div>
             <div class="my-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="thumbnail">Thumbnail</label>
                 <input class="border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="thumbnail" type="file" accept="image/*">
+                    id="thumbnail" name="thumbnail" type="file" accept="image/*">
             </div>
-            <div class="my-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="name" type="text" placeholder="Name">
-            </div>
-            <div class="my-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description</label>
-                <textarea
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="description" placeholder="Description"></textarea>
-            </div>
-            <div class="my-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="amount">Amount</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="amount" type="number" placeholder="Amount">
-            </div>
+            <button href="" type="submit"
+            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            Simpan
+            </button>
         </form>
     </main>
 @endsection
--- resources/views/admin/zakat-sekarang/index.blade.php
+++ resources/views/admin/zakat-sekarang/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <!-- Main Content -->
     <main class="max-w-screen-xl flex-auto m-10"> <!-- Tambahkan kelas "flex justify-end" -->
-        <h1 class="text-2xl font-semibold mb-4">List of Users</h1>
+        <h1 class="text-2xl font-semibold mb-4">Data Zakat Sekarang</h1>
         <a href="{{ route('admin.dashboard.zakat-sekarang.create') }}" type="button"
             class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             Tambah Data
@@ -24,31 +24,31 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                     <tr>
                         <th scope="col" class="px-5 py-3">
-                            Name
+                            Judul
                         </th>
                         <th scope="col" class="px-5 py-3">
-                            Email
+                            Jenis Zakat
                         </th>
                         <th scope="col" class="px-5 py-3">
-                            Role
+                            Jumlah
                         </th>
                         <th scope="col" class="px-5 py-3">
-                            Actions
+                            Aksi
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Loop through your users and display them here -->
-                    <tr class="bg-white border-b text-black ">
-                        <td class="px-5 py-4 ">John Doe</td>
-                        <td class="px-5 py-4 ">john@example.com</td>
-                        <td class="px-5 py-4 ">Admin</td>
-                        <td class="px-5 py-4 ">
-                            <a href="#" class="text-blue-500 hover:underline">Edit</a>
-                            <a href="#" class="text-red-500 hover:underline ml-2" data-modal-toggle="deleteModal">Delete</a>
-                        </td>
-                    </tr>
-                    <!-- Add more rows as needed -->
+                    @foreach ($zakat_sekarang as $zakat)
+                        <tr class="bg-white border-b text-black ">
+                            <td class="px-5 py-4 ">{{ $zakat->name }}</td>
+                            <td class="px-5 py-4 ">{{ $zakat->type_id }}</td>
+                            <td class="px-5 py-4 ">Rp{{ $zakat->amount }}</td>
+                            <td class="px-5 py-4 ">
+                                <a href="#" class="text-blue-500 hover:underline">Edit</a>
+                                <a href="#" class="text-red-500 hover:underline ml-2" data-modal-toggle="deleteModal">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
