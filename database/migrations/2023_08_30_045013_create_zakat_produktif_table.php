cb090e0625238c898555780bda1ffdbb51d9e151
Syahravi <syahravi.id@gmail.com>
2023-08-30 12:46:15 +0700

    Zakat

---
+++ database/migrations/2023_08_30_045013_create_zakat_produktif_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('zakat_produktif', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('description');
+            $table->string('business');
+            $table->decimal('amount', 15, 2);
+            $table->uuid('thumbnail_id');
+            $table->uuid('type_id');
+            $table->uuid('admin_id');
+            $table->uuid('mustahik_id');
+            $table->uuid('amil_id');
+            $table->foreign('thumbnail_id')->references('id')->on('files');
+            $table->foreign('type_id')->references('id')->on('zakat_categories');
+            $table->foreign('admin_id')->references('id')->on('users');
+            $table->foreign('mustahik_id')->references('id')->on('users');
+            $table->foreign('amil_id')->references('id')->on('users');
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('zakat_produktifs');
+    }
+};
