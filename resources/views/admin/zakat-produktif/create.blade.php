f09bb33a7fd428107ff0e433d2395e4ed5fad9f7
Syahravi <syahravi.id@gmail.com>
2023-08-30 13:16:33 +0700

    tab

--- resources/views/admin/zakat-produktif/create.blade.php
+++ resources/views/admin/zakat-produktif/create.blade.php
@@ -37,7 +37,7 @@
             <button href="" type="submit"
             class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             Simpan
-        </button>
+            </button>
         </form>
     </main>
 @endsection
